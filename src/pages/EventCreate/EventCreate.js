12e95c79b9e7934efa463066c07a05c594d1aecd
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-10-01 07:38:34 +0600

    added heading and global padding in event create page

--- src/pages/EventCreate/EventCreate.js
+++ src/pages/EventCreate/EventCreate.js
@@ -6,12 +6,15 @@ import addPhotoImg from "../../Assets/file.png";
 const EventCreate = () => {
     const navigate = useNavigate();
     return (
-        <div>
+        <div className="md:p-[26px]">
             <form className="">
-                <div className="md:flex min-h-[80vh] items-center">
-                    <div className="flex-1 flex flex-col items-center justify-center">
+                <div className="flex flex-col-reverse lg:flex-row min-h-[80vh] items-center">
+                    <div className="flex-1 flex flex-col items-center justify-center w-full px-2 my-8 lg:my-0">
+                        <h1 className=" text-[25px] lg:text-[50px] w-full font-bold">
+                            Create your event
+                        </h1>
                         {/*input field*/}
-                        <div className="flex flex-col">
+                        <div className="flex flex-col w-full">
                             <label htmlFor="name" className="ml-2 my-2">
                                 🎉 My event is called
                             </label>
@@ -19,32 +22,32 @@ const EventCreate = () => {
                                 type="text"
                                 name="name"
                                 id="name"
-                                className="bg-gray-200 lg:w-[718px] py-3 px-2 text-gray-600 outline-none focus:bg-purple2 rounded-[4px]"
+                                className="bg-gray-200 w-[100%] lg:w-[718px] py-3 px-2 text-gray-600 outline-none focus:bg-purple2 rounded-[4px] w-full"
                             />
                         </div>
-                        <div className="flex flex-col">
+                        <div className="flex flex-col w-full">
                             <label htmlFor="name" className="ml-2 my-2">
                                 🗓 It starts at
                             </label>
                             <input
-                                className="bg-gray-200 lg:w-[718px] py-3 px-2 text-gray-600 outline-none focus:bg-purple2 rounded-[4px]"
+                                className="bg-gray-200 w-full lg:w-[718px] py-3 px-2 text-gray-600 outline-none focus:bg-purple2 rounded-[4px]"
                                 type="date"
                                 name=""
                                 id="startDate"
                             />
                         </div>
-                        <div className="flex flex-col">
+                        <div className="flex flex-col w-full">
                             <label htmlFor="name" className="ml-2 my-2">
                                 🏁 It ends at (optional)
                             </label>
                             <input
-                                className="bg-gray-200 lg:w-[718px] py-3 px-2 text-gray-600 outline-none focus:bg-purple2 rounded-[4px]"
+                                className="bg-gray-200 lg:w-[718px] py-3 px-2 text-gray-600 outline-none focus:bg-purple2 rounded-[4px] w-full"
                                 type="date"
                                 name=""
                                 id="startDate"
                             />
                         </div>
-                        <div className="flex flex-col">
+                        <div className="flex flex-col w-full">
                             <label htmlFor="name" className="ml-2 my-2">
                                 📍 It’s happening at (optional)
                             </label>
@@ -52,29 +55,29 @@ const EventCreate = () => {
                                 type="text"
                                 name="name"
                                 id="name"
-                                className="bg-gray-200 lg:w-[718px] py-3 px-2 text-gray-600 outline-none focus:bg-purple2 rounded-[4px]"
+                                className="bg-gray-200 lg:w-[718px] py-3 px-2 text-gray-600 outline-none focus:bg-purple2 rounded-[4px] w-full"
                             />
                         </div>
-                        <div className="flex flex-col">
+                        <div className="flex flex-col w-full">
                             <label htmlFor="name" className="ml-2 my-2">
-                                🎉 My event is called
+                                🔗 Add a URL link (optional)
                             </label>
                             <input
                                 type="text"
                                 name="name"
                                 id="name"
-                                className="bg-gray-200 lg:w-[718px] py-3 px-2 text-gray-600 outline-none focus:bg-purple2 rounded-[4px]"
+                                className="bg-gray-200 lg:w-[718px] py-3 px-2 text-gray-600 outline-none focus:bg-purple2 rounded-[4px] w-full"
                             />
                         </div>
-                        <div className="flex flex-col">
+                        <div className="flex flex-col w-full">
                             <label htmlFor="name" className="ml-2 my-2">
-                                🎉 My event is called
+                                ✏️ Description (optional)
                             </label>
                             <input
                                 type="text"
                                 name="name"
                                 id="name"
-                                className="bg-gray-200 lg:w-[718px] py-3 px-2 text-gray-600 outline-none focus:bg-purple2 rounded-[4px]"
+                                className="bg-gray-200 lg:w-[718px] py-3 px-2 text-gray-600 outline-none focus:bg-purple2 rounded-[4px] w-full"
                             />
                         </div>
                     </div>
@@ -94,7 +97,7 @@ const EventCreate = () => {
                         />
                     </div>
                 </div>
-                <div className="flex justify-center w-full">
+                <div className="flex justify-center w-full mt-[20px] md:mt-0">
                     <input
                         type="submit"
                         className="py-3 px-8 rounded-[4px] bg-gradient-to-r from-invitedPurple to-purple1 text-gray-100"
